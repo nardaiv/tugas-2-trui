f961595b006263bceda860f6608c33ee9ab9f9c8
nardaiv <nardaikiru@gmail.com>
2024-11-24 19:28:32 +0700

    create structure of robotpart

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
+class RobotPart{
+private:
+    std::string partName;
+    int pwmValue;
+
+public:
+    RobotPart(const std::string& name, int pwm)
+        : partName(name), pwmValue(pwm) {}
+    
+    // destructor
+    virtual ~RobotPart() = default;
+
+    virtual void performAction() = 0;
+
+    void screamTopic(){
+        std::cout << "This is " <<  partName << "dengan nilai PWM: " << pwmValue << std::endl;
+
+    } 
+
+};
 
 int main(){
 
