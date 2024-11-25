81dd7ca2d487cbfb95a4eaac38e762e8d5cc711e
nardaiv <nardaikiru@gmail.com>
2024-11-25 11:16:30 +0700

    add RobotLeftArm and RobotRightArm

--- main.cpp
+++ main.cpp
@@ -45,6 +45,32 @@ public:
 
 };
 
+class RobotLeftArm : public RobotPart{
+private:
+    bool hasSensor;
+
+public:
+    RobotLeftArm(bool sensor, int pwm) 
+        : RobotPart("Left Arm", pwm), hasSensor(sensor) {};
+
+    void performAction() override{
+        std::cout << getPartName() << ": Menyeimbangkan dengan status sensor: " << (hasSensor ? "Aktif" : "Tidak Aktif") << " dan PWM: "<< getPwmValue() <<std::endl;
+    }
+};
+
+class RobotRightArm : public RobotPart{
+private:
+    bool canGrab;
+
+public:
+    RobotRightArm(bool grab, int pwm)
+        : RobotPart("Right Arm", pwm), canGrab(grab) {}
+
+    void performAction() override{
+        std::cout << getPartName() << ": Menggenggam objek dengan tangan:: " << (canGrab ? "Bisa" : "Tidak Bisa") << " dan PWM: "<< getPwmValue() <<std::endl;
+    }
+};
+
 int main(){
 
     return 0;
